e472d20b2047c78dc152c88153bd5244f2c39af4
jvfiel <jvfiel@bai.ph>
2018-01-22 23:25:46 +0800

    using abbyy

--- erpnext_ocr/erpnext_ocr/xml_reader.py
+++ erpnext_ocr/erpnext_ocr/xml_reader.py
@@ -1,125 +1,97 @@
-import xml.etree.cElementTree as ET
 
-source = '/home/jvfiel/frappe-bl3ndlabs/apps/erpnext_ocr/erpnext_ocr/erpnext_ocr/test.xml'
-tree = ET.ElementTree(file=source)
-root = tree.getroot()
+def read():
+    import xml.etree.cElementTree as ET
 
-# for child_of_root in root:
-#     print child_of_root.tag, child_of_root.attrib
+    source = '/home/jvfiel/frappe-bl3ndlabs/apps/erpnext_ocr/erpnext_ocr/erpnext_ocr/test.xml'
+    tree = ET.ElementTree(file=source)
+    root = tree.getroot()
 
-# for elem in tree.iter():
-#     # print elem.tag, elem.attrib
-#     # print elem
-#     print elem.attrib,elem.tail
-#     appt_children = elem.getchildren()
-#     # for child in appt_children:
-#     #     print elem.attrib, elem.tail
+    xmlname = root.tag.split("}")
+    # print xmlname
+    xmlname = xmlname[0]
+    xmlname += "}"
+    # print xmlname
 
-# print root.tag
-xmlname = root.tag.split("}")
-# print xmlname
-xmlname = xmlname[0]
-xmlname += "}"
-# print xmlname
+    #Vendor Name
+    vendor_name = tree.findall('.//{0}vendor//{0}name//{0}recognizedValue//{0}text'.format(xmlname))[0].text
+        # print elem.tag, elem.attrib, elem.text
+    #Vendor Address
+    vendor_address = tree.findall('.//{0}vendor//{0}address//{0}text'.format(xmlname))[0].text
+    #    print elem.tag, elem.attrib, elem.text
 
-#Vendor Name
-for elem in tree.findall('.//{0}vendor//{0}name//{0}recognizedValue//{0}text'.format(xmlname)):
-    print elem.tag, elem.attrib, elem.text
-#Vendor Address
-for elem in tree.findall('.//{0}vendor//{0}address//{0}text'.format(xmlname)):
-    print elem.tag, elem.attrib, elem.text
+    #Vendor Phone
+    vendor_phone = tree.findall('.//{0}vendor//{0}phone//{0}recognizedValue//{0}text'.format(xmlname))[0].text
+    #    print elem.tag, elem.attrib, elem.text
 
-#Vendor Phone
-for elem in tree.findall('.//{0}vendor//{0}phone//{0}recognizedValue//{0}text'.format(xmlname)):
-    print elem.tag, elem.attrib, elem.text
-
-# Vendor Phone
-for elem in tree.findall('.//{0}vendor//{0}fax//{0}recognizedValue//{0}text'.format(xmlname)):
-    print elem.tag, elem.attrib, elem.text
-
-# Vendor PurchaseType
-for elem in tree.findall('.//{0}vendor//{0}purchaseType'.format(xmlname)):
-    print elem.tag, elem.attrib, elem.text
-
-# Vendor Date
-for elem in tree.findall('.//{0}date//{0}text'.format(xmlname)):
-    print elem.tag, elem.attrib, elem.text
-
-# # Vendor Time
-# for elem in tree.findall('.//{0}time//{0}recognizedValue//{0}text'.format(xmlname)):
-#     print elem.tag, elem.attrib, elem.text
-
-# Vendor subTotal
-for elem in tree.findall('.//{0}subTotal//{0}text'.format(xmlname)):
-    print elem.tag, elem.attrib, elem.text
-
-# # Vendor Total
-# for elem in tree.findall('.//{0}total//{0}text'.format(xmlname)):
-#     print elem.tag, elem.attrib, elem.text
-
-# Vendor Payment Card Number
-for elem in tree.findall('.//{0}payment//{0}cardNumber'.format(xmlname)):
-    print elem.tag, elem.attrib, elem.text
-
-# Vendor Payment Value
-for elem in tree.findall('.//{0}payment//{0}value//{0}recognizedValue'.format(xmlname)):
-    print elem.tag, elem.attrib, elem.text
-
-# Vendor Items
-for elem in tree.findall('.//{0}recognizedItems//{0}item'.format(xmlname)):
-    # print elem.tag, elem.attrib, elem.text, "*", elem.attrib['index']
-    i = elem.attrib['index']
-    # for elem in tree.findall('.//{0}recognizedItems//{0}item[@index="{1}"]//{0}name//{0}text'.format(xmlname,i)):
+    # Vendor Fax
+    # for elem in tree.findall('.//{0}vendor//{0}fax//{0}recognizedValue//{0}text'.format(xmlname)):
     #     print elem.tag, elem.attrib, elem.text
 
+    # Vendor PurchaseType
+    vendor_purchasetype = tree.findall('.//{0}vendor//{0}purchaseType'.format(xmlname))[0].text
+    #    print elem.tag, elem.attrib, elem.text
 
-    elem = tree.findall('.//{0}recognizedItems//{0}item[@index="{1}"]//{0}name//{0}text'.format(xmlname, i))[0]
-    print elem.tag, elem.attrib, elem.text
-    elem = tree.findall('.//{0}recognizedItems//{0}item[@index="{1}"]//{0}total//{0}recognizedValue//{0}text'.format(xmlname, i))[0]
-    print elem.tag, elem.attrib, elem.text
-    elem = tree.findall('.//{0}recognizedItems//{0}item[@index="{1}"]//{0}count//{0}normalizedValue'.format(xmlname, i))[0]
-    print elem.tag, elem.attrib, elem.text
+    # Vendor Date
+    invoice_date = tree.findall('.//{0}date//{0}text'.format(xmlname))[0].text
+    #    print elem.tag, elem.attrib, elem.text
 
-
-    # children = elem.getchildren()
-    # for child in children:
+    # # Vendor Time
+    # for elem in tree.findall('.//{0}time//{0}recognizedValue//{0}text'.format(xmlname)):
     #     print elem.tag, elem.attrib, elem.text
 
-# for elem in tree.findall('.//{http://www.abbyy.com/ReceiptCaptureSDK_xml/ReceiptCapture-1.0.xsd}name'):
-#     print elem.tag, elem.attrib, elem.text
+    # Vendor subTotal
+    vendor_subTotal = tree.findall('.//{0}subTotal//{0}text'.format(xmlname))[0].text
+    #    print elem.tag, elem.attrib, elem.text
 
-# appointments = root.getchildren()
-# for appointment in appointments:
-#     print appointment.attrib
-#     appt_children = appointment.getchildren()
-#     print "-",appointment.tag
-#
-#     for appt_child in appt_children:
-#         # print "%s=%s" % (appt_child.tag, appt_child.text)
-#         print "*",appt_child.attrib,appt_child.tag
-#
-#         for appt_child_1 in appt_child.getchildren():
-#             print "**",appt_child_1.attrib, appt_child_1.tag
-#
-#             for appt_child_2 in appt_child_1.getchildren():
-#
-#                 findtext = appt_child_2.text
-#
-#                 if findtext:
-#                     findtext = findtext.strip()
-#
-#                 if findtext:
-#
-#                     print "***",appt_child_2.attrib, appt_child_2.tag, appt_child_2.text
-#                 else:
-#                     for appt_child_3 in appt_child_2.getchildren():
-#                         print "***>", appt_child_3.attrib, appt_child_3.tag, appt_child_3.text
-#
-#
-#                     # for apppp in appt_child_1.getchildren():
-#             #     print apppp.text
-#     # print "-------------------------"
-#
-# # for attrName, attrValue in root.attributes.items():
-# #     #do whatever you'd like
-# #     print "attribute %s = %s" % (attrName, attrValue)
+    # # Vendor Total
+    # for elem in tree.findall('.//{0}total//{0}text'.format(xmlname)):
+    #     print elem.tag, elem.attrib, elem.text
+
+    # Vendor Payment Card Number
+    # payment_cardno = tree.findall('.//{0}payment//{0}cardNumber'.format(xmlname))[0].text
+    #    print elem.tag, elem.attrib, elem.text
+
+    # Vendor Payment Value
+    payment_val = tree.findall('.//{0}payment//{0}value//{0}recognizedValue//{0}text'.format(xmlname))[0].text
+    #    print elem.tag, elem.attrib, elem.text
+
+    items = []
+
+    # Vendor Items
+    for elem in tree.findall('.//{0}recognizedItems//{0}item'.format(xmlname)):
+
+
+        # print elem.tag, elem.attrib, elem.text, "*", elem.attrib['index']
+        i = elem.attrib['index']
+        # for elem in tree.findall('.//{0}recognizedItems//{0}item[@index="{1}"]//{0}name//{0}text'.format(xmlname,i)):
+        #     print elem.tag, elem.attrib, elem.text
+
+        #NAME
+        elem = tree.findall('.//{0}recognizedItems//{0}item[@index="{1}"]//{0}name//{0}text'.format(xmlname, i))[0]
+        # print elem.tag, elem.attrib, elem.text
+        name = elem.text
+        #TOTAL
+        elem = tree.findall('.//{0}recognizedItems//{0}item[@index="{1}"]//{0}total//{0}recognizedValue//{0}text'.format(xmlname, i))[0]
+        # print elem.tag, elem.attrib, elem.text
+        total = elem.text
+        #COUNT
+        elem = tree.findall('.//{0}recognizedItems//{0}item[@index="{1}"]//{0}count//{0}normalizedValue'.format(xmlname, i))[0]
+        # print elem.tag, elem.attrib, elem.text
+        count = elem.text
+        items.append({"item_code":name,"qty":count,"total":total})
+
+    print items
+
+    return {
+        "vendor_name":vendor_name,
+        "vendor_address":vendor_address,
+        "vendor_phone":vendor_phone,
+        "vendor_address":vendor_address,
+        "invoice_date":invoice_date,
+        "subTotal":vendor_subTotal,
+        # "payment_cardno":payment_cardno,
+        "payment_val":payment_val,
+        "items":items
+    }
+
+print read()
