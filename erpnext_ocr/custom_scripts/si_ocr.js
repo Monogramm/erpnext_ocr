ceaa548c7aef1e2f6b21fbc84d126440010d1b7b
jvfiel <jvfiel@bai.ph>
2018-02-12 01:39:52 +0800

    using https://github.com/justinchmura/js-treeview as treeview in SI

--- erpnext_ocr/custom_scripts/si_ocr.js
+++ erpnext_ocr/custom_scripts/si_ocr.js
@@ -1,3 +1,32 @@
+  frappe.ui.form.on("Sales Invoice","onload",function (frm) {
+                              frappe.call({
+                            method: "erpnext_ocr.erpnext_ocr.xml_reader.read",
+                            args: {
+                                "ocr_receipt": cur_frm.doc.ocr_receipt
+                            },
+                            callback: function (r, rt) {
+
+       var tree = new TreeView(r.message, 'tree');
+
+                                tree.on('select', function (e) {
+                                    console.log(e);
+    console.log(JSON.stringify(e));
+                                    // var copyTextarea = document.querySelector(e.target);
+                                    // copyTextarea.select();
+
+                                   var range = window.getSelection().getRangeAt(0);
+        range.selectNode(e.target.srcElement);
+        window.getSelection().addRange(range);
+        document.execCommand("copy")
+
+});
+
+
+                            }
+                        });
+  });
+
+
    frappe.ui.form.on("Sales Invoice","ocr_receipt",function(frm){
 
 
@@ -41,16 +70,32 @@
         refresh: function (frm) {
 
 
+             frappe.call({
+                            method: "erpnext_ocr.erpnext_ocr.xml_reader.read",
+                            args: {
+                                "ocr_receipt": cur_frm.doc.ocr_receipt
+                            },
+                            callback: function (r, rt) {
+
+       var tree = new TreeView(r.message, 'tree');
+
+                                tree.on('select', function (e) {
+                                    console.log(e);
+    console.log(JSON.stringify(e));
+                                    // var copyTextarea = document.querySelector(e.target);
+                                    // copyTextarea.select();
+
+                                   var range = window.getSelection().getRangeAt(0);
+        range.selectNode(e.target.srcElement);
+        window.getSelection().addRange(range);
+        document.execCommand("copy")
+
+});
 
 
-var tree = new TreeView([
-    { name: 'Item 1', children: [] },
-    { name: 'Item 2', expanded: true, children: [
-            { name: 'Sub Item 1', children: [] },
-            { name: 'Sub Item 2', children: [] }
-        ]
-    }
-], 'tree');
+                            }
+                        });
+
 
         }
 
