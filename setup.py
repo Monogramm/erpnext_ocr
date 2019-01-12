621dc8d7212098eed7918985dc3d3a8c394823ae
John Vincent Fiel <johnvincentfiel@gmail.com>
2019-01-13 00:29:49 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+   from pip.req import parse_requirements
 import re, ast
+
 
 # get version from __version__ variable in erpnext_ocr/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
