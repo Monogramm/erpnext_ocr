fc2b966a169ec376a4bb6f5c8fe42ac3be58645f
mathieu.brunot <mathieu.brunot@monogramm.io>
2019-08-12 09:26:02 +0200

    :fire: Remove all ABBYY OCR and Sales Invoice code
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- erpnext_ocr/erpnext_ocr/doctype/ocr_receipt/ocr_receipt.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2018, John Vincent Fiel and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('OCR Receipt', {
-	refresh: function(frm) {
-
-	}
-});
