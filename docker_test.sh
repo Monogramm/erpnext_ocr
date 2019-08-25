e36b7db6a2098ad91119993acaf2e75d5cb731f1
mathieu.brunot <mathieu.brunot@monogramm.io>
2019-08-25 23:00:28 +0200

    :green_heart: Fix docket test exit codes
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- docker_test.sh
+++ docker_test.sh
@@ -2,27 +2,27 @@
 
 if [ ! -f "${FRAPPE_WD}/sites/apps.txt" ] || [ ! -f "${FRAPPE_WD}/sites/.docker-app-init" ]; then
     echo 'Apps were not installed in time!'
-    exit -1
+    exit 1
 fi
 
 if [ ! -f "${FRAPPE_WD}/sites/currentsite.txt" ] || [ ! -f "${FRAPPE_WD}/sites/.docker-site-init" ]; then
     echo 'Site was not installed in time!'
-    exit -2
+    exit 2
 fi
 
 if [ ! sudo ping -c 10 -q erpnext_db ]; then
     echo 'ERPNext database container is not responding!'
-    exit -4
+    exit 4
 fi
 
 if [ ! sudo ping -c 10 -q erpnext_app ]; then
     echo 'ERPNext app container is not responding!'
-    exit -8
+    exit 8
 fi
 
 if [ ! sudo ping -c 10 -q erpnext_web ]; then
     echo 'ERPNext web container is not responding!'
-    exit -16
+    exit 16
 fi
 
 ## Unit testing:
