a5ec08cd412fc534753e686067c19e8cd978d8d3
mathieu.brunot <mathieu.brunot@monogramm.io>
2019-08-26 16:47:03 +0200

    :construction: Strict docker test script
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- docker_test.sh
+++ docker_test.sh
@@ -1,28 +1,30 @@
 #!/usr/bin/sh
 
+set -e
+
 echo "Waiting to ensure everything is fully ready for the tests..."
 sleep 60
 
 echo "Checking content of sites directory..."
-if [ ! -f "${FRAPPE_WD}/sites/apps.txt" ] || [ ! -f "${FRAPPE_WD}/sites/.docker-app-init" ] || [ ! -f "${FRAPPE_WD}/sites/currentsite.txt" ] || [ ! -f "${FRAPPE_WD}/sites/.docker-site-init" ] || [ ! -f "${FRAPPE_WD}/sites/.docker-init" ]; then
+if [ ! -f "./sites/apps.txt" ] || [ ! -f "./sites/.docker-app-init" ] || [ ! -f "./sites/currentsite.txt" ] || [ ! -f "./sites/.docker-site-init" ] || [ ! -f "./sites/.docker-init" ]; then
     echo 'Apps and site are not initalized?!'
-    ls -al "${FRAPPE_WD}/sites"
+    ls -al "./sites"
     # FIXME We couldn't be running tests if those files did not existd... so why are they not visible?!
     #exit 1
 fi
 
 echo "Checking main containers are reachable..."
-if [ ! sudo ping -c 10 -q frappe_db ]; then
+if ! sudo ping -c 10 -q frappe_db ; then
     echo 'Database container is not responding!'
     exit 2
 fi
 
-if [ ! sudo ping -c 10 -q frappe_app ]; then
+if ! sudo ping -c 10 -q frappe_app ; then
     echo 'App container is not responding!'
     exit 4
 fi
 
-if [ ! sudo ping -c 10 -q frappe_web ]; then
+if ! sudo ping -c 10 -q frappe_web ; then
     echo 'Web container is not responding!'
     exit 8
 fi
