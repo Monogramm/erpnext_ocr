9c6f1d76a7e1c376d7059e3df3491cc603664a95
Emil <amionv99@gmail.com>
2019-11-10 23:45:18 +0300

    :children_crossing: Changed txt file on API method
    Signed-off-by: Emil <amionv99@gmail.com>

--- erpnext_ocr/erpnext_ocr/doctype/ocr_language/ocr_language.py
+++ erpnext_ocr/erpnext_ocr/doctype/ocr_language/ocr_language.py
@@ -8,17 +8,14 @@ import os
 
 import frappe
 from frappe.model.document import Document
-
+from tesserocr import PyTessBaseAPI # For library should download `apt-get install libleptonica-dev libtesseract-dev`
 
 @frappe.whitelist()
 def lang_is_supported(lang):
-    print(frappe.get_site_path())
-    with open(frappe.get_site_path()+"/../../apps/erpnext_ocr/erpnext_ocr/supported_lang.txt", "r") as f:
-        for line in f:
-            if lang in line:
-                return "Yes"
-        return "No"
-
+    if lang == 'en':
+        lang = "eng"
+    with PyTessBaseAPI(path='/usr/share/tesseract-ocr/4.00/tessdata/', lang='eng') as api:
+        return "Yes" if lang in api.GetAvailableLanguages() else "No"
 
 class OCRLanguage(Document):
     pass
