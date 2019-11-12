fd9f59030d248f7e3c694e618421b331a4c41c95
mathieu.brunot <mathieu.brunot@monogramm.io>
2019-11-12 19:13:38 +0100

    :white_check_mark: Add basic OCR Read tests
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- erpnext_ocr/erpnext_ocr/doctype/ocr_read/test_ocr_read.py
+++ erpnext_ocr/erpnext_ocr/doctype/ocr_read/test_ocr_read.py
@@ -6,5 +6,38 @@ from __future__ import unicode_literals
 import frappe
 import unittest
 
+def create_ocr_reads():
+    if frappe.flags.test_ocr_reads_created:
+        return
+
+    frappe.set_user("Administrator")
+    doc = frappe.get_doc({
+        "doctype": "OCR Read",
+        "file_to_read":"sample1.jpg",
+        "language": "eng"
+    }).insert()
+
+    doc = frappe.get_doc({
+        "doctype": "OCR Read",
+        "file_to_read":"sample2.pdf",
+        "language": "eng"
+    }).insert()
+
+    frappe.flags.test_ocr_reads_created = True
+
+
 class TestOCRRead(unittest.TestCase):
-	pass
+    def setUp(self):
+        create_ocr_reads()
+
+    def tearDown(self):
+        frappe.set_user("Administrator")
+
+    def test_ocr_read_list(self):
+        frappe.set_user("test1@example.com")
+        res = frappe.get_list("OCR Read", filters=[["OCR Read", "file_to_read", "like", "sample%"]], fields=["name", "file_to_read"])
+        self.assertEquals(len(res), 2)
+        files_to_read = [r.file_to_read for r in res]
+        self.assertTrue("sample1.jpg" in files_to_read)
+        self.assertTrue("sample2.pdf" in files_to_read)
+
