323a2749e74ac4e0a5b700786625499084cdcc13
mathieu.brunot <mathieu.brunot@monogramm.io>
2019-11-13 17:44:28 +0100

    :green_heart: Remove XML reports
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- .travis/docker_test.sh
+++ .travis/docker_test.sh
@@ -49,8 +49,46 @@ FRAPPE_APP_TO_TEST=erpnext_ocr
 #bench run-tests --help
 echo "Executing Unit Tests of '${FRAPPE_APP_TO_TEST}' app..."
 bench run-tests \
-    --app ${FRAPPE_APP_TO_TEST} \
-    --junit-xml-output "${FRAPPE_APP_TO_TEST}_unit_tests.xml"
+    --app ${FRAPPE_APP_TO_TEST}
+
+# FIXME Frappe 11+ / Python 3.7 issue when generating --junit-xml-output "${FRAPPE_APP_TO_TEST}_unit_tests.xml"
+# Traceback (most recent call last):
+#   File "/usr/local/lib/python3.7/runpy.py", line 193, in _run_module_as_main
+#     "__main__", mod_spec)
+#   File "/usr/local/lib/python3.7/runpy.py", line 85, in _run_code
+#     exec(code, run_globals)
+#   File "/home/frappe/frappe-bench/apps/frappe/frappe/utils/bench_helper.py", line 97, in <module>
+#     main()
+#   File "/home/frappe/frappe-bench/apps/frappe/frappe/utils/bench_helper.py", line 18, in main
+#     click.Group(commands=commands)(prog_name='bench')
+#   File "/home/frappe/frappe-bench/env/lib/python3.7/site-packages/click/core.py", line 764, in __call__
+#     return self.main(*args, **kwargs)
+#   File "/home/frappe/frappe-bench/env/lib/python3.7/site-packages/click/core.py", line 717, in main
+#     rv = self.invoke(ctx)
+#   File "/home/frappe/frappe-bench/env/lib/python3.7/site-packages/click/core.py", line 1137, in invoke
+#     return _process_result(sub_ctx.command.invoke(sub_ctx))
+#   File "/home/frappe/frappe-bench/env/lib/python3.7/site-packages/click/core.py", line 1137, in invoke
+#     return _process_result(sub_ctx.command.invoke(sub_ctx))
+#   File "/home/frappe/frappe-bench/env/lib/python3.7/site-packages/click/core.py", line 956, in invoke
+#     return ctx.invoke(self.callback, **ctx.params)
+#   File "/home/frappe/frappe-bench/env/lib/python3.7/site-packages/click/core.py", line 555, in invoke
+#     return callback(*args, **kwargs)
+#   File "/home/frappe/frappe-bench/env/lib/python3.7/site-packages/click/decorators.py", line 17, in new_func
+#     return f(get_current_context(), *args, **kwargs)
+#   File "/home/frappe/frappe-bench/apps/frappe/frappe/commands/__init__.py", line 25, in _func
+#     ret = f(frappe._dict(ctx.obj), *args, **kwargs)
+#   File "/home/frappe/frappe-bench/apps/frappe/frappe/commands/utils.py", line 419, in run_tests
+#     ui_tests = ui_tests, doctype_list_path = doctype_list_path, failfast=failfast)
+#   File "/home/frappe/frappe-bench/apps/frappe/frappe/test_runner.py", line 70, in main
+#     ret = run_all_tests(app, verbose, profile, ui_tests, failfast=failfast)
+#   File "/home/frappe/frappe-bench/apps/frappe/frappe/test_runner.py", line 118, in run_all_tests
+#     out = unittest_runner(verbosity=1+(verbose and 1 or 0), failfast=failfast).run(test_suite)
+#   File "/home/frappe/frappe-bench/env/lib/python3.7/site-packages/xmlrunner/runner.py", line 116, in run
+#     result.generate_reports(self)
+#   File "/home/frappe/frappe-bench/env/lib/python3.7/site-packages/xmlrunner/result.py", line 657, in generate_reports
+#     test_runner.output.write(xml_content)
+# TypeError: write() argument must be str, not bytes
+#    --junit-xml-output "${FRAPPE_APP_TO_TEST}_unit_tests.xml"
 
 ## TODO Check result of tests
 #cat "${FRAPPE_APP_TO_TEST}_unit_tests.xml"
