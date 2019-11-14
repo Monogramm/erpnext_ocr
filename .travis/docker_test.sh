7ae3afe47333ac8829f19900a65d97e38f9ce488
mathieu.brunot <mathieu.brunot@monogramm.io>
2019-11-14 12:40:02 +0100

    :chart_with_upward_trend: Use absolute path to app
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- .travis/docker_test.sh
+++ .travis/docker_test.sh
@@ -90,7 +90,7 @@ fi
 
 if [ -f ./sites/.coverage ]; then
     echo "Sending Unit Tests coverage of '${FRAPPE_APP_TO_TEST}' app to Coveralls..."
-    coveralls -b apps/${FRAPPE_APP_TO_TEST} -d ./sites/.coverage
+    coveralls -b "$(pwd)/apps/${FRAPPE_APP_TO_TEST}" -d ./sites/.coverage
 fi
 
 
