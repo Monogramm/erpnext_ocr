33a78c2cabd6542e3ec118f4d3bcdb2e0f527103
Emil <amionv99@gmail.com>
2019-11-14 12:56:24 +0300

    :art: Update readable javascipt
    Signed-off-by: Emil <amionv99@gmail.com>

--- erpnext_ocr/erpnext_ocr/doctype/ocr_language/ocr_language.js
+++ erpnext_ocr/erpnext_ocr/doctype/ocr_language/ocr_language.js
@@ -2,15 +2,15 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('OCR Language', "lang", function (frm) {
-        if (typeof cur_frm.doc.lang != 'undefined') {
+        if (typeof frm.doc.lang != 'undefined') {
             frappe.call({
                 args: {
-                    "lang": cur_frm.doc.lang
+                    "lang": frm.doc.lang
                 },
                 method: "erpnext_ocr.erpnext_ocr.doctype.ocr_language.ocr_language.check_language_web",
                 callback: function (r) {
                     console.log(r.text);
-                    cur_frm.set_value("is_supported", r.message);
+                    frm.set_value("is_supported", r.message);
                 }
             });
         }
