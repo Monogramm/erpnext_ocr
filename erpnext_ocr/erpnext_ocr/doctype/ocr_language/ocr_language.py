4eb7ceef94095065b5685f1a9ac19deda87fbb24
Emil <amionv99@gmail.com>
2019-11-15 03:54:52 +0300

    :art: Remove static path
    Signed-off-by: Emil <amionv99@gmail.com>

--- erpnext_ocr/erpnext_ocr/doctype/ocr_language/ocr_language.py
+++ erpnext_ocr/erpnext_ocr/doctype/ocr_language/ocr_language.py
@@ -4,6 +4,7 @@
 
 from __future__ import unicode_literals
 
+import os
 
 import frappe
 from frappe.model.document import Document
@@ -11,16 +12,19 @@ from frappe.model.document import Document
 # This library requires leptonica and tesseract-dev
 from tesserocr import PyTessBaseAPI
 
+
 @frappe.whitelist()
 def check_language_web(lang):
     return "Yes" if lang_is_supported(lang) else "No"
+
 
 @frappe.whitelist()
 def lang_is_supported(lang):
     if lang == 'en':
         lang = "eng"
-    with PyTessBaseAPI(path='/usr/share/tesseract-ocr/4.00/tessdata/', lang='eng') as api:
-        return lang in api.GetAvailableLanguages()
+    list_of_languages = tesserocr.get_languages()[1]
+    return lang in list_of_languages
+
 
 class OCRLanguage(Document):
     pass
