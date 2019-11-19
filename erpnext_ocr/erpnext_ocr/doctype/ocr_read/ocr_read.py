2540871866ce6fb842438aecc2faf6d345f1b99e
mathieu.brunot <mathieu.brunot@monogramm.io>
2019-11-19 17:41:26 +0100

    :lipstick: Improve progress evolution
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- erpnext_ocr/erpnext_ocr/doctype/ocr_read/ocr_read.py
+++ erpnext_ocr/erpnext_ocr/doctype/ocr_read/ocr_read.py
@@ -52,17 +52,20 @@ def read_document(path, lang='eng'):
         pdf = wi(filename=fullpath, resolution=300)
         pdf_image = pdf.convert('jpeg')
         i = 0
-        size = len(pdf_image.sequence)
+        size = len(pdf_image.sequence) * 3
         for img in pdf_image.sequence:
             img_page = wi(image=img)
             image_blob = img_page.make_blob('jpeg')
+            frappe.publish_realtime("ocr_progress_bar", {"progress": [i, size]}, user=frappe.session.user)
+            i += 1
 
             recognized_text = " "
-
             image = Image.open(io.BytesIO(image_blob))
+            frappe.publish_realtime("ocr_progress_bar", {"progress": [i, size]}, user=frappe.session.user)
+            i += 1
+
             recognized_text = pytesseract.image_to_string(image, lang)
             text = text + recognized_text
-
             frappe.publish_realtime("ocr_progress_bar", {"progress": [i, size]}, user=frappe.session.user)
             i += 1
 
@@ -74,7 +77,7 @@ def read_document(path, lang='eng'):
         frappe.publish_realtime("ocr_progress_bar", {"progress": [66, 100]}, user=frappe.session.user)
 
     text.split(" ")
-    frappe.publish_realtime("ocr_progress_bar", {"progress": "1"}, user=frappe.session.user)
+    frappe.publish_realtime("ocr_progress_bar", {"progress": [100, 100]}, user=frappe.session.user)
 
     return text
 
