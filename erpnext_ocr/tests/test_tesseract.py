097a378c1ecf93a421bf9110fda0c7fa1ae1ad9c
Emil <amionv99@gmail.com>
2019-11-19 13:24:32 +0300

    :wrench: Add env variable for tests
    Signed-off-by: Emil <amionv99@gmail.com>

--- erpnext_ocr/tests/test_tesseract.py
+++ erpnext_ocr/tests/test_tesseract.py
@@ -3,12 +3,14 @@
 # See license.txt
 from __future__ import unicode_literals
 
+import locale
 import unittest, os
 
 from erpnext_ocr.erpnext_ocr.doctype.ocr_read.ocr_read import read_document
 
 class TestTesseract(unittest.TestCase):
     def test_read_document_image(self):
+        locale.setlocale(locale.LC_ALL, 'C')
         recognized_text = read_document(os.path.join(os.path.dirname(__file__),
                                         "test_data", "sample1.jpg"),
                             "eng")
@@ -26,6 +28,7 @@ class TestTesseract(unittest.TestCase):
         self.assertTrue(recognized_text == expected_text)
 
     def test_read_document_pdf(self):
+        locale.setlocale(locale.LC_ALL, 'C')
         recognized_text = read_document(os.path.join(os.path.dirname(__file__),
                                         "test_data", "sample2.pdf"),
                             "eng")
