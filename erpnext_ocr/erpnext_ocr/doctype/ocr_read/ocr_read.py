c9c9b79358ff28fbd82f0650f19db6803ab2f05a
AminovE99 <32329685+AminovE99@users.noreply.github.com>
2019-11-21 14:13:23 +0300

    :lipstick: Improve progress evolution (#11)
    * :construction: Attempt of fix the progress bar
    
    Signed-off-by: Emil <amionv99@gmail.com>
    
    * :lipstick: Improve progress evolution
    
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- erpnext_ocr/erpnext_ocr/doctype/ocr_read/ocr_read.py
+++ erpnext_ocr/erpnext_ocr/doctype/ocr_read/ocr_read.py
@@ -53,6 +53,7 @@ def read_document(path, lang='eng'):
         # external link
         fullpath = requests.get(path, stream=True).raw
 
+    frappe.publish_realtime("ocr_progress_bar", {"progress": "0"}, user=frappe.session.user)
     text = " "
 
     if path.endswith('.pdf'):
@@ -60,28 +61,32 @@ def read_document(path, lang='eng'):
         pdf = wi(filename=fullpath, resolution=300)
         pdf_image = pdf.convert('jpeg')
         i = 0
-        size = len(pdf_image.sequence)
+        size = len(pdf_image.sequence) * 3
         for img in pdf_image.sequence:
             img_page = wi(image=img)
             image_blob = img_page.make_blob('jpeg')
+            frappe.publish_realtime("ocr_progress_bar", {"progress": [i, size]}, user=frappe.session.user)
+            i += 1
 
             recognized_text = " "
-
             image = Image.open(io.BytesIO(image_blob))
+            frappe.publish_realtime("ocr_progress_bar", {"progress": [i, size]}, user=frappe.session.user)
+            i += 1
+
             recognized_text = pytesseract.image_to_string(image, lang)
             text = text + recognized_text
-
-            frappe.publish_realtime("ocr_progress_bar", {"progress": [i, size]})
+            frappe.publish_realtime("ocr_progress_bar", {"progress": [i, size]}, user=frappe.session.user)
             i += 1
 
     else:
-        frappe.publish_realtime("ocr_progress_bar", {"progress": "0"}, user=frappe.session.user)
-
         image = Image.open(fullpath)
+        frappe.publish_realtime("ocr_progress_bar", {"progress": [33, 100]}, user=frappe.session.user)
 
         text = pytesseract.image_to_string(image, lang=lang)
+        frappe.publish_realtime("ocr_progress_bar", {"progress": [66, 100]}, user=frappe.session.user)
 
     text.split(" ")
+    frappe.publish_realtime("ocr_progress_bar", {"progress": [100, 100]}, user=frappe.session.user)
 
     return text
 
