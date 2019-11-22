10479cd51cfb2fda54c99b32057741b779db0312
mathieu.brunot <mathieu.brunot@monogramm.io>
2019-11-22 01:22:36 +0100

    :bulb: Update frappe docs config
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- erpnext_ocr/config/docs.py
+++ erpnext_ocr/config/docs.py
@@ -2,14 +2,14 @@
 Configuration for docs
 """
 
-# source_link = "https://github.com/[org_name]/erpnext_ocr"
-# docs_base_url = "https://[org_name].github.io/erpnext_ocr"
-# headline = "App that does everything"
-# sub_heading = "Yes, you got that right the first time, everything"
+source_link = "https://github.com/Monogramm/erpnext_ocr"
+docs_base_url = ""
+headline = "ERPNext OCR Integration"
+sub_heading = "Optical Character Recognition using tesseract within ERPNext"
 
 def get_context(context):
 	context.brand_html = "ERPNext OCR"
-	context.source_link = "https://github.com/Monogramm/erpnext_ocr"
-	context.docs_base_url = "https://github.com/Monogramm/erpnext_ocr"
-	context.headline = "OCR Integration"
-	context.sub_heading = "Optical Character Recognition using tesseract within ERPNext"
+	context.source_link = source_link
+	context.docs_base_url = docs_base_url
+	context.headline = headline
+	context.sub_heading = sub_heading
