2230f250286bbec057e8e21c024c9439024a95b9
mathieu.brunot <mathieu.brunot@monogramm.io>
2019-11-27 00:17:54 +0100

    :construction: Text based Doctype import
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

---
+++ erpnext_ocr/erpnext_ocr/doctype/ocr_import/test_ocr_import.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019, Monogramm and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import frappe
+import unittest
+
+class TestOCRImport(unittest.TestCase):
+	pass
