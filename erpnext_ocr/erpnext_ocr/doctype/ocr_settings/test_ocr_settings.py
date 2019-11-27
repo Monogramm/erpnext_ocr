7577108cadeb24b1eb757ab63a11b55ab346ce85
mathieu.brunot <mathieu.brunot@monogramm.io>
2019-11-27 15:02:01 +0100

    :white_check_mark: Add test on OCR Settings validate
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- erpnext_ocr/erpnext_ocr/doctype/ocr_settings/test_ocr_settings.py
+++ erpnext_ocr/erpnext_ocr/doctype/ocr_settings/test_ocr_settings.py
@@ -12,6 +12,7 @@ class TestOCRSettings(unittest.TestCase):
         ocr = frappe.get_doc("OCR Settings")
         ocr.pdf_resolution = 300
         ocr.validate()
+        self.assertEqual(300, ocr.pdf_resolution)
 
     def test_validate_invalid_pdf_resolution(self):
         ocr = frappe.get_doc("OCR Settings")
