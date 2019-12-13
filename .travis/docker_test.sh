8f4d748e0537ce23ca2a7f7ae9bf2aefd7223ec0
mathieu.brunot <mathieu.brunot@monogramm.io>
2019-12-14 00:05:52 +0100

    :construction: Display available commands
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- .travis/docker_test.sh
+++ .travis/docker_test.sh
@@ -126,13 +126,14 @@ fi
 ################################################################################
 # TODO Generate docs
 
-bench build-docs --help
+#bench build-docs --help
+bench --help
 
 #echo "Generating docs for '${FRAPPE_APP_TO_TEST}' app..."
 #if [ "${TEST_VERSION}" = "10" ] || [ "${TEST_VERSION}" = "11" ]; then
 #    bench build-docs --target ${FRAPPE_APP_TO_TEST} ${FRAPPE_APP_TO_TEST}
 #else
-#    bench build-docs --target ${FRAPPE_APP_TO_TEST} ${FRAPPE_APP_TO_TEST}
+#    bench write-docs --target ${FRAPPE_APP_TO_TEST} ${FRAPPE_APP_TO_TEST}
 #fi
 
 ## TODO Check docs generated properly
