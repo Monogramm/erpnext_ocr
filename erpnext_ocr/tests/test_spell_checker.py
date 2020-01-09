7bcfb675c347046bd6af4a86c40c85539f2a43ca
mathieu.brunot <mathieu.brunot@monogramm.io>
2020-01-09 13:32:55 +0100

    :art: Rename spell check tests
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- erpnext_ocr/tests/test_spell_checker.py
+++ erpnext_ocr/tests/test_spell_checker.py
@@ -3,10 +3,10 @@ import unittest
 from erpnext_ocr.erpnext_ocr.doctype.ocr_read.ocr_read import get_spellchecked_text, get_words_from_text
 
 
-class MyTestCase(unittest.TestCase):
+class TestSpellChecker(unittest.TestCase):
     def test_spell_checker(self):
-        text = get_spellchecked_text("An exampel. I beleive these text is not corect.", "eng")
-        self.assertEqual("An example. I believe these text is not correct.", text)
+        text = get_spellchecked_text("An exampel. I beleive this text is not corect.", "eng")
+        self.assertEqual("An example. I believe this text is not correct.", text)
 
     def test_get_words_from_text(self):
         words = get_words_from_text("Cat in gloves. Catches: no mice.")
