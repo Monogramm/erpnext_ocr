f307903680665aa58279ddd8fb6ef48c6a3ee7e3
Emil <amionv99@gmail.com>
2020-01-14 01:04:04 +0300

    :white_check_mark: Add test for creation ocr language v2
    Signed-off-by: Emil <amionv99@gmail.com>

--- erpnext_ocr/erpnext_ocr/doctype/ocr_language/test_ocr_language.py
+++ erpnext_ocr/erpnext_ocr/doctype/ocr_language/test_ocr_language.py
@@ -11,6 +11,21 @@ from erpnext_ocr.erpnext_ocr.doctype.ocr_language.ocr_language import lang_avail
 
 
 class TestOCRLanguage(unittest.TestCase):
+    def setUp(self):
+        frappe.set_user("Administrator")
+        frappe.get_doc({
+            "doctype": "OCR Language",
+            "code": "sin",
+            "lang": "si"
+        }).insert()
+        frappe.flags.test_ocr_language_created = True
+    def tearDown(self):
+        if frappe.flags.test_ocr_language_created:
+            frappe.set_user("Administrator")
+            frappe.get_doc("OCR Language", "sin").delete()
+        frappe.flags.test_ocr_language_created = False
+
+
     def test_en_lang_available(self):
         self.assertTrue(lang_available("en"))
 
@@ -40,10 +55,3 @@ class TestOCRLanguage(unittest.TestCase):
 
     def test_666_check_language(self):
         self.assertEqual(check_language("666"), frappe._("No"))
-
-    def test_creation_of_ocr_language(self):
-        frappe.set_user("Administrator")
-        frappe.get_doc({
-            "doctype": "OCR Language",
-            "lang": "en"
-        }).insert()
