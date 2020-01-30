ff1956adb57aae1227e448b9d5d24a53783f4e8c
Emil <amionv99@gmail.com>
2020-01-30 16:51:45 +0300

    :construction: Update test and add ability to generate tables
    Signed-off-by: Emil <amionv99@gmail.com>

--- erpnext_ocr/erpnext_ocr/doctype/ocr_import/ocr_import.py
+++ erpnext_ocr/erpnext_ocr/doctype/ocr_import/ocr_import.py
@@ -17,16 +17,30 @@ def generate_doctype(doctype_import_link, read_result):
 	doctype_import_doc = frappe.get_doc("OCR Import", doctype_import_link)
 	generated_doc = frappe.get_doc({"doctype": doctype_import_link})
 	list_with_errors = []
+	list_with_table_values = []
 	for field in doctype_import_doc.mappings:
 		try:
 			found_field = re.search(field.regexp, read_result)
 			if found_field is not None:
-				generated_doc.__dict__[field.field] = re.findall(field.regexp, read_result).pop(0)
+				if field.is_table:
+					table_doc = frappe.get_doc({"doctype": doctype_import_doc})
+					ocr_import_table = frappe.get_doc("OCR Import", field.link_to_child_doc)
+					string = re.findall(field.regexp, read_result).pop(0)
+					for mapped_value in ocr_import_table.mappings:
+						table_doc.__dict__[mapped_value.field] = re.findall(mapped_value.regexp, string).pop(0)
+					table_doc.save()
+					list_with_table_values.append(table_doc)
+					generated_doc.__dict__[field.field] = list_with_table_values
+				else:
+					generated_doc.__dict__[field.field] = re.findall(field.regexp, read_result).pop(0)
 			else:
 				frappe.throw(frappe._("Cannot find field {0} in text").format(field.field))
 		except KeyError:
 			list_with_errors.append("Field {} doesn't exist in doctype".format(doctype_import_doc))
 	if list_with_errors:
 		frappe.throw(list_with_errors)
-	generated_doc.save()
+	try:
+		generated_doc.save()
+	except frappe.exceptions.DuplicateEntryError:
+		frappe.throw("Generated doc is already exist")
 	return generated_doc
--- erpnext_ocr/erpnext_ocr/doctype/ocr_import/test_ocr_import.py
+++ erpnext_ocr/erpnext_ocr/doctype/ocr_import/test_ocr_import.py
@@ -3,6 +3,7 @@
 # See license.txt
 from __future__ import unicode_literals
 
+import datetime
 import os
 import unittest
 
@@ -11,27 +12,52 @@ from erpnext_ocr.erpnext_ocr.doctype.ocr_import.ocr_import import generate_docty
 
 
 class TestOCRImport(unittest.TestCase):
-    def test_generating_doctype(self):
+    def setUp(self):
         frappe.set_user("Administrator")
-        item_code_mapping = frappe.get_doc(
-            {"doctype": "OCR Import Mapping", "field": "item_code", "regexp": "Item Code (\\w+)"})
-        item_group_mapping = frappe.get_doc(
-            {"doctype": "OCR Import Mapping", "field": "item_group", "regexp": "Item Group (\\w+)"})
-        list_with_ocr_import = []
-        list_with_ocr_import.append(item_code_mapping)
-        list_with_ocr_import.append(item_group_mapping)
-        ocr_import = frappe.get_doc(
-            {"doctype": "OCR Import", "name": "Item", "mappings": list_with_ocr_import, 'doctype_link': "Item"})
-        ocr_read_doctype = frappe.get_doc(
+        self.item_ocr_read = frappe.get_doc(
             {"doctype": "OCR Read", "file_to_read": os.path.join(os.path.dirname(__file__),
                                                                  os.path.pardir, os.path.pardir,
                                                                  os.path.pardir,
                                                                  "tests", "test_data",
                                                                  "item.pdf"), "language": "eng"})
-        ocr_import.save()
-        ocr_read_doctype.ocr_import = ocr_import.name
-        ocr_read_doctype.save()
-        ocr_read_doctype.read_image()
-        generated_item = generate_doctype(ocr_import.name, ocr_read_doctype.read_result)
+        self.item_ocr_import = frappe.new_doc("OCR Import")
+        self.item_ocr_import.name = "Item"
+        self.item_ocr_import.doctype_link = "Item"
+        self.item_ocr_import.insert(ignore_permissions=True)
+        self.item_ocr_import.save()
+        self.item_ocr_read.ocr_import = self.item_ocr_import.name
+        self.item_ocr_read.read_image()
+
+        self.sales_invoice_ocr_import = frappe.new_doc("OCR Import")
+        self.sales_invoice_ocr_import.doctype_link = "Sales Invoice"
+        self.sales_invoice_ocr_import.name = "Sales Invoice"
+        self.sales_invoice_ocr_import.save()
+
+        self.sales_invoice_ocr_read = frappe.get_doc(
+            {"doctype": "OCR Read", "file_to_read": os.path.join(os.path.dirname(__file__),
+                                                                 os.path.pardir, os.path.pardir,
+                                                                 os.path.pardir,
+                                                                 "tests", "test_data",
+                                                                 "Picture_010.png"), "language": "eng"})
+
+    def tearDown(self):
+        self.item_ocr_read.delete()
+        self.item_ocr_import.delete()
+
+    def test_generating_item(self):
+        item_code_mapping = frappe.get_doc(
+            {"doctype": "OCR Import Mapping", "field": "item_code", "regexp": "Item Code (\\w+)",
+             "parenttype": "OCR Import", "parent": "Item"})
+        item_code_mapping.save(ignore_permissions=True)
+        item_group_mapping = frappe.get_doc(
+            {"doctype": "OCR Import Mapping", "field": "item_group", "regexp": "Item Group (\\w+)",
+             "parenttype": "OCR Import", "parent": "Item"})
+        item_group_mapping.save(ignore_permissions=True)
+        self.item_ocr_import.append("mappings", item_code_mapping.__dict__)
+        self.item_ocr_import.append("mappings", item_group_mapping.__dict__)
+        self.item_ocr_import.save()
+        generated_item = generate_doctype(self.item_ocr_import.name, self.item_ocr_read.read_result)
         self.assertEqual(generated_item.item_code, "fdsa")
         self.assertEqual(generated_item.item_group, "Consumable")
+
+#    def test_generating_sales_invoice(self):
