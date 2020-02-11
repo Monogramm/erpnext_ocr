4dd806ceff2f549d922d1ac0fb869f1c0313c2e9
Emil <amionv99@gmail.com>
2020-02-12 01:44:12 +0300

    :lipstick: Devide some lines
    Signed-off-by: Emil <amionv99@gmail.com>

--- erpnext_ocr/erpnext_ocr/doctype/ocr_import/ocr_import.py
+++ erpnext_ocr/erpnext_ocr/doctype/ocr_import/ocr_import.py
@@ -18,26 +18,34 @@ class OCRImport(Document):
 
 
 @frappe.whitelist()
-def generate_child_doctype(doctype_import_link, field, string_raw_table_value, doctype_import_doc, table_doc):
+def generate_child_doctype(doctype_import_link, string_raw_table_value, table_doc):
+    """
+    Generate child of some doctype
+    :param doctype_import_link: link to OCR Import
+    :param string_raw_table_value: String for future child
+    :param doctype_import_doc:
+    :param table_doc:
+    :return:
+    """
     ocr_import_table = frappe.get_doc("OCR Import",
                                       doctype_import_link)
     for table_field in ocr_import_table.mappings:
-        found_field = find_field(table_field, string_raw_table_value)  # table_doc can be here
+        found_field = find_field(table_field, string_raw_table_value)
         if found_field is not None:
             table_doc.__dict__[table_field.field] = found_field
             raw_date = table_doc.__dict__[table_field.field]
             if table_field == 'Date':
-                format_from_settings = frappe.get_doc("System Settings").date_format
-                table_doc.__dict__[table_field.field] = datetime.strptime(raw_date,
-                                                                          PYTHON_FORMAT[format_from_settings])
-    table_doc.parent = doctype_import_doc.name
+                format_date = frappe.get_doc("System Settings").date_format
+                table_doc.__dict__[table_field.field] = datetime.strptime(raw_date, PYTHON_FORMAT[format_date])
+    table_doc.parent = ocr_import_table.name
     table_doc.save()
     return table_doc
 
 
 def find_field(field, read_result):
     if field.value_type == "Python":
-        found_field = eval(field.value)  # we can't use ast.literal_eval, because we use strings of code in field.value
+        # we can't use ast.literal_eval, because we use strings of code in field.value
+        found_field = eval(field.value)
     else:
         if field.value_type == "Regex group":
             pattern_result = re.findall(field.regexp, read_result)
@@ -55,15 +63,14 @@ def generate_doctype(doctype_import_link, read_result):
     list_with_table_values = []
     for field in doctype_import_doc.mappings:
         try:
-            found_field = find_field(field, read_result)  # generated_doc can be send to find_field
+            found_field = find_field(field, read_result)
             if found_field is not None:
                 if field.value_type == "Table":
                     iter_of_str = re.finditer(field.regexp, read_result)
                     for item_match in iter_of_str:
                         raw_table_doc = generated_doc.append(field.field)
                         item_str = item_match.group()
-                        table_doc = generate_child_doctype(field.link_to_child_doc, field, item_str,
-                                                           doctype_import_doc,
+                        table_doc = generate_child_doctype(field.link_to_child_doc, item_str,
                                                            raw_table_doc)
                         list_with_table_values.append(table_doc)
                         generated_doc.__dict__[field.field] = list_with_table_values
@@ -74,9 +81,11 @@ def generate_doctype(doctype_import_link, read_result):
                 else:
                     generated_doc.__dict__[field.field] = found_field
             else:
-                frappe.throw(frappe._("Cannot find field {0} in text").format(field.field))
+                frappe.throw(
+                    frappe._("Cannot find field {0} in text").format(field.field))
         except KeyError:
-            list_with_errors.append("Field {} doesn't exist in doctype".format(doctype_import_doc))
+            list_with_errors.append("Field {} doesn't exist in doctype".
+                                    format(doctype_import_doc))
     if list_with_errors:
         frappe.throw(list_with_errors)
     try:
